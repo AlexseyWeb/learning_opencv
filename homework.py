bcd7f29e574eadec4078e4c394247e468059578d
Алексей Гусаков <alongenet32@gmail.com>
2024-08-10 20:45:17 +0300

    loop open photos

--- homework.py
+++ homework.py
@@ -1,8 +1,10 @@
 import cv2 as cv 
 
-img = cv.imread("Photos/cat.jpg")
 
-cv.imshow("Cat", img)
+list_of_images = ["cat.jpg", "funny_dog.jpeg", "white_dog.jpeg"]
+
+for index, img in enumerate(list_of_images):
+	cv.imshow(f"{index}",cv.imread(f"Photos/{img}"))
 
 
 cv.waitKey(0)
