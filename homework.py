53846bf412d8b362d41c1f636c867a79695bbd44
Алексей Гусаков <alongenet32@gmail.com>
2024-08-10 22:25:23 +0300

    refactoring homework.py

--- homework.py
+++ homework.py
@@ -1,10 +1,32 @@
 import cv2 as cv 
+""" Samples """
 
 
-list_of_images = ["cat.jpg", "funny_dog.jpeg", "white_dog.jpeg"]
+def show_images():
+	"""Show all images of folder """
+	list_of_images = ["cat.jpg", "funny_dog.jpeg", "white_dog.jpeg"]
 
-for index, img in enumerate(list_of_images):
-	cv.imshow(f"{index}",cv.imread(f"Photos/{img}"))
+	for index, img in enumerate(list_of_images):
+		cv.imshow(f"{index}",cv.imread(f"Photos/{img}"))
+
+	cv.waitKey(0)
+
+def show_videos():
+	"""Show videos of folder """
+	capture = cv.VideoCapture("Videos/dog.mp4")
+	while True:
+		_, frame = capture.read()
+		cv.imshow("Video", frame)
+		if cv.waitKey(20) & 0xFF == ord('d'):
+			break
+
+	capture.release()
+	cv.destroyAllWindows()
 
 
-cv.waitKey(0)
+
+
+
+
+
+
