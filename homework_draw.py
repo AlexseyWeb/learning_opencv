f70154ee2c3cde7c7e66a5739c8c6b584406088b
Алексей Гусаков <alongenet32@gmail.com>
2024-08-11 18:56:39 +0300

    functions circle, line, rectangle, putText

---
+++ homework_draw.py
@@ -0,0 +1,19 @@
+import cv2 as cv 
+import numpy as np 
+
+blank = np.zeros((400, 400, 3), np.uint8)
+font = cv.FONT_HERSHEY_SIMPLEX
+
+cv.line(blank, (200, 200), (100, 100), (255, 0, 255), thickness=1)
+
+
+for i in range(0, 150):
+	cv.line(blank, (i, i + 10), (100, i), (255, 0, 255), thickness=1)
+	cv.rectangle(blank, (i + 10, i), (i, 100), (0, 255, 0), 3)
+
+
+cv.putText(blank, "OpenCV", (120, 80), font, 2, (255, 255, 255), 2, cv.LINE_AA)
+cv.circle(blank,(200, 200), 20, (0, 0, 255), -1) 
+cv.imshow("Draw", blank)
+
+cv.waitKey(0)
