7204bd9b5de195a291f70a95fbfcd0e27a2ef2f6
Алексей Гусаков <alongenet32@gmail.com>
2024-08-12 06:58:04 +0300

    add text to image

--- homework_draw.py
+++ homework_draw.py
@@ -1,6 +1,7 @@
 import cv2 as cv 
 import numpy as np 
 
+img = cv.imread("Photos/cat.jpg")
 blank = np.zeros((400, 400, 3), np.uint8)
 font = cv.FONT_HERSHEY_SIMPLEX
 
@@ -15,5 +16,8 @@ for i in range(0, 150):
 cv.putText(blank, "OpenCV", (120, 80), font, 2, (255, 255, 255), 2, cv.LINE_AA)
 cv.circle(blank,(200, 200), 20, (0, 0, 255), -1) 
 cv.imshow("Draw", blank)
+
+cv.putText(img, "Cat", (img.shape[1] // 2, img.shape[0] // 2), font, 2, (0, 0, 33), 2, cv.LINE_AA)
+cv.imshow("Lable", img)
 
 cv.waitKey(0)
