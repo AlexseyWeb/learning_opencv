d196020a7439db03e0c7f19d8fd9ac2477efae1d
Алексей Гусаков <alongenet32@gmail.com>
2024-08-17 21:53:02 +0300

    added photo and functions translation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2 as cv 
 
 img = cv.imread("Photos/cat.jpg")
-cv.imshow("Title", img)
+canny = cv.Canny(img, 200, 200)
+resize_canny = cv.resize(canny, (500, 500), interpolation=cv.INTER_LINEAR)
+cv.imshow("Title", resize_canny)
+
 
 cv.waitKey(0)
