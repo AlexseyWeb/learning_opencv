9b666928e64fe3b9a46371d70cec03bc88510811
Алексей Гусаков <alongenet32@gmail.com>
2024-08-17 06:32:51 +0300

    add standarts functions

--- samples/esentials_function.py
+++ samples/esentials_function.py
@@ -1,6 +1,6 @@
 import cv2 as cv 
 
-img = cv.imread("../Photos/funny_dog.jpeg")
+img = cv.imread("../Photos/white_dog.jpeg")
 cv.imshow("Original", img)
 
 #GRAY IMAGE
@@ -11,6 +11,24 @@ cv.imshow("Gray Photo", gray)
 blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
 cv.imshow("Blur", blur)
 
+#Edge Cascade
+canny = cv.Canny(img, 125, 170)
+cv.imshow("Edge Cascade", canny)
 
+#Dilating the image
+dilated = cv.dilate(img, (3, 3), iterations=1)
+cv.imshow("Dilated", dilated)
+
+#Eroding
+eroded = cv.erode(img, (7,7), iterations=3)
+cv.imshow("Erode", eroded)
+
+#Resize
+resized = cv.resize(img, (50, 50), interpolation=cv.INTER_CUBIC)
+cv.imshow("Resized", resized)
+
+#Cropping
+cropped = img[50:255, 150:155]
+cv.imshow("Cropped", cropped) 
 
 cv.waitKey(0)
