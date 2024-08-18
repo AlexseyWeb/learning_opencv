644e55d0513241a45cc84acf8845d2d0af44eb6f
Алексей Гусаков <alongenet32@gmail.com>
2024-08-18 07:03:35 +0300

    add function rotation

--- samples/image_transformations.py
+++ samples/image_transformations.py
@@ -18,7 +18,20 @@ def translate(img, x, y):
 
 
 translated = translate(img, -100, -100)
+
+#Rotation
+def rotation(img, angle, rPoint=None):
+    (height, width) = img.shape[:2]
+    if rPoint is None:
+        rPoint = (width // 2, height // 2)
+    
+    rotMath = cv.getRotationMatrix2D(rPoint, angle, 1.0)
+    dimension = (width, height)
+
+    return cv.warpAffine(img, rotMath, dimension)
+
+rotated = rotation(img, 90)
     
 
-cv.imshow("Dog", translated)
+cv.imshow("Dog", rotated)
 cv.waitKey(0)
