bd77925898dd2a72ad26e85cdd6a3a722a8f535c
Алексей Гусаков <alongenet32@gmail.com>
2024-08-27 23:11:29 +0300

    changed detector, now smile on photos

--- samples/face_detector.py
+++ samples/face_detector.py
@@ -1,6 +1,6 @@
 import cv2 as cv 
 
-img = cv.imread("../Photos/man.jpg")
+img = cv.imread("../Photos/smile.jpeg")
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 haar_cascade = cv.CascadeClassifier("har_detector.xml")
@@ -8,8 +8,8 @@ face_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 haar_face_cascade = cv.CascadeClassifier("har_face_detector.xml")
 face_front_detector = haar_face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
-print(f"Number of faces = {len(face_front_detector)}")
-for (x, y, w, h) in face_front_detector:
+print(f"Number of faces = {len(face_rect)}")
+for (x, y, w, h) in face_rect:
     cv.rectangle(img, (x,y), (x + w, y + h), (0, 255, 0), 2)
 
 cv.imshow("Face detector", img)
