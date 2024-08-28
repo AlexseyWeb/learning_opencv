f6cccbfd596cb461054bfbaf5abe37623dff0878
Алексей Гусаков <alongenet32@gmail.com>
2024-08-28 07:01:58 +0300

    changed minNeighbors count

--- samples/face_detector.py
+++ samples/face_detector.py
@@ -1,15 +1,15 @@
 import cv2 as cv 
 
-img = cv.imread("../Photos/smile.jpeg")
+img = cv.imread("../Photos/man.jpg")
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 haar_cascade = cv.CascadeClassifier("har_detector.xml")
-face_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+face_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=7)
 haar_face_cascade = cv.CascadeClassifier("har_face_detector.xml")
-face_front_detector = haar_face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+face_front_detector = haar_face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=8)
 
-print(f"Number of faces = {len(face_rect)}")
-for (x, y, w, h) in face_rect:
+print(f"Number of faces = {len(face_front_detector)}")
+for (x, y, w, h) in face_front_detector:
     cv.rectangle(img, (x,y), (x + w, y + h), (0, 255, 0), 2)
 
 cv.imshow("Face detector", img)
