94c2b079c3696aa71f61cd7a5a4840b27803bd40
Алексей Гусаков <alongenet32@gmail.com>
2024-08-29 06:59:21 +0300

    added corner

---
+++ samples/detector_corners.py
@@ -0,0 +1,15 @@
+import cv2 as cv 
+import numpy as np 
+
+img = cv.imread("../Photos/man.jpg")
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+corners = cv.goodFeaturesToTrack(gray, 7, 0.05, 25)
+corners = np.float32(corners)
+
+for item in corners:
+    x, y = item[0]
+    cv.circle(img,  (x, y), 5, (0, 0, 255), -1)
+
+cv.imshow("Top", img)
+cv.waitKey(0)
